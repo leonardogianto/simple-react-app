ea79c2a434ca0896b77033412f651b5fe5ba15c3
Leonardo Gianto <lgianto16@gmail.com>
2023-06-14 15:39:42 +0700

    First commit

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,13 @@
+export default function Contact() {
+  return (
+    <>
+      <div className="contact-title">THIS IS THE CONTACT PAGE</div>
+      <div className="contact-subtitle">
+        <p>
+          Hi there! Feel free to use the contact form on the right to reach out,
+        </p>
+        <p>or write the old fashion way.</p>
+      </div>
+    </>
+  );
+}
