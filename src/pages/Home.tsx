ba359dfdf8ec69fc9f4d29573270a590f6934283
Leonardo Gianto <lgianto16@gmail.com>
2023-06-18 22:45:15 +0700

    updated header and footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="homepage">
       <h1>Welcome to my site. This is the home page.</h1>
       <img
         src="./src/images/321_EL_Leonardo Gianto.jpg"
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,13 @@
 import { Outlet, Link } from "react-router-dom";
 import "../index.css";
+import Switch from "../components/Switch";
+import { useState } from "react";
 
 const Layout = () => {
+  const [value, setValue] = useState(false);
   return (
     <>
-      <div className="toplayouts">
+      <div className="navbar">
         <div className="myname">
           <h4>LEONARDO GIANTO</h4>
         </div>
@@ -25,8 +28,10 @@ const Layout = () => {
             </button>
           </Link>
         </div>
-        <Outlet />
+        <Switch isOn={value} handleToggle={() => setValue(!value)} />
       </div>
+      <Outlet />
+      <div className="footer">Bottom box</div>
     </>
   );
 };
